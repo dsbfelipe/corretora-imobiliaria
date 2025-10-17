5ae4b0a987b3e1b96110c9291f8acec18252bea7
Felipe dos Santos Bento <dsbfelipe@outlook.com>
2025-10-17 15:31:00 -0300

    feat: add class Proprietario

---
+++ src/main/java/fpbo/models/Proprietario.java
@@ -0,0 +1,25 @@
+package fpbo.models;
+
+public class Proprietario {
+  private final String nome;
+  private final String telefone;
+  private final String cpf;
+
+  public Proprietario(String nome, String telefone, String cpf) {
+    this.nome = nome;
+    this.telefone = telefone;
+    this.cpf = cpf;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public String getTelefone() {
+    return telefone;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+}
